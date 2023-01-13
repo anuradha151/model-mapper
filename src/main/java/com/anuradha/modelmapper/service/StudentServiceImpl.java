e8dc923383390383a32dc33bf7e63761be23735f
Anuradha Ranasinghe <anuradha@arimaclanka.com>
2023-01-14 00:15:46 +0530

    Feature : simple mapping added

--- src/main/java/com/anuradha/modelmapper/service/StudentServiceImpl.java
+++ src/main/java/com/anuradha/modelmapper/service/StudentServiceImpl.java
@@ -1,20 +1,23 @@
 package com.anuradha.modelmapper.service;
 
 import com.anuradha.modelmapper.dto.StudentDto;
+import com.anuradha.modelmapper.entity.Student;
 import com.anuradha.modelmapper.repo.StudentRepository;
+import lombok.AllArgsConstructor;
+import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
 
 @Service
+@AllArgsConstructor
 public class StudentServiceImpl implements StudentService {
 
     private final StudentRepository studentRepository;
-
-    public StudentServiceImpl(StudentRepository studentRepository) {
-        this.studentRepository = studentRepository;
-    }
+    private final ModelMapper modelMapper;
 
     @Override
     public StudentDto save(StudentDto studentDto) {
-        return null;
+        Student student = modelMapper.map(studentDto, Student.class);
+        studentRepository.save(student);
+        return modelMapper.map(student, StudentDto.class);
     }
 }
